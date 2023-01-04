bde16f0b28204d2cc787eb89d9fd2d0e96caa87b
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-01-03 21:56:02 -0500

    added star svg

--- src/features/retaurants/components/restaurant-info-card.component.js
+++ src/features/retaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { Text, Card, Paragraph } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 //How to use styled components
 //For all specified component
@@ -32,6 +34,12 @@ const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
@@ -43,11 +51,18 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos }} />
       <Info>
         <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
