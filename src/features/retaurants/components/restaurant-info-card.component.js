5c9e6ea13ff1f1242693487df43e3414ca381b72
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-01-04 16:14:03 -0500

    applied RCTView fix for Android

--- src/features/retaurants/components/restaurant-info-card.component.js
+++ src/features/retaurants/components/restaurant-info-card.component.js
@@ -73,7 +73,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Title>{name}</Title>
         <Section>
           <Rating>
-            {ratingArray.map((id) => (
+            {ratingArray.map(() => (
               <SvgXml xml={star} width={20} height={20} />
             ))}
           </Rating>
@@ -83,10 +83,12 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <Spacer variant={"left.large"} />
-            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            <Spacer variant={"left.large"} />
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            <Spacer position="left" size="small">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="small">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
           </SectionEnd>
         </Section>
         <Address>{address}</Address>
