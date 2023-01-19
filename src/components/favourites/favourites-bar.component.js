d94aeb1f16dfb5a61701751e5bfd25b978c17575
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-01-18 19:09:28 -0500

    about to start with local storage

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -9,24 +9,29 @@ const FavouritesWrapper = styled.View`
   padding: 10px;
 `;
 
-export const FavouritesBar = ({ favourites, onNavigate }) => (
-  <FavouritesWrapper>
-    <Spacer variant="left.large">
-      <Text variant="caption">Favourites</Text>
-    </Spacer>
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {favourites.map((restaurant) => {
-        const key = restaurant.name;
-        return (
-          <Spacer position="left" size="medium" key={key}>
-            <TouchableOpacity
-              onPress={() => onNavigate("RestaurantDetail", restaurant)}
-            >
-              <CompactRestaurantInfo restaurant={restaurant} />
-            </TouchableOpacity>
-          </Spacer>
-        );
-      })}
-    </ScrollView>
-  </FavouritesWrapper>
-);
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (!favourites.length) {
+    return null;
+  }
+  return (
+    <FavouritesWrapper>
+      <Spacer variant="left.large">
+        <Text variant="caption">Favourites</Text>
+      </Spacer>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {favourites.map((restaurant) => {
+          const key = restaurant.name;
+          return (
+            <Spacer position="left" size="medium" key={key}>
+              <TouchableOpacity
+                onPress={() => onNavigate("RestaurantDetail", { restaurant })}
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+            </Spacer>
+          );
+        })}
+      </ScrollView>
+    </FavouritesWrapper>
+  );
+};
