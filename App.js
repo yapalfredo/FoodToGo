8f528fa066e9bc2b51b50c03178eff3f6eb68c01
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-01-19 22:46:24 -0500

    started creating authentication service

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import { LocationContextProvider } from "./src/services/location/location.context";
 //Loading google fonts
@@ -15,6 +14,7 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCotc07DQmmieLcQ3g8sfPGDbdTBRfVpAQ",
@@ -26,23 +26,10 @@ const firebaseConfig = {
 };
 
 if (!getApps().length) {
-  const app = initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
 }
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    signInWithEmailAndPassword(getAuth(), "test@test.com", "test123")
-      .then((userCredential) => {
-        const user = userCredential.user;
-        setIsAuthenticated(true);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
-
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -53,22 +40,22 @@ export default function App() {
 
   if (!oswaldLoaded || !latoLoaded) null;
 
-  if (!isAuthenticated) null;
-
   return (
     // <> allows you wrap multiple components
     // side by side without having to wrap them
     // with a specific container
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
-        <ExpoStatusBar style="auto" />
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+          <ExpoStatusBar style="auto" />
+        </AuthenticationContextProvider>
       </ThemeProvider>
     </>
   );
---
+++ src/services/authentication/authentication.service.js
@@ -0,0 +1,8 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth/react-native";
+
+export const loginRequest = (email, password) => {
+  signInWithEmailAndPassword(getAuth(), email, password);
+};
