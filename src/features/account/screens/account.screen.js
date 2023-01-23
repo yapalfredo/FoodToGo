6f62c333e1d5d3cf99938a98af2bd8a9064ddd7d
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-01-22 20:05:48 -0500

    worked on startup screens

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton onPress={() => navigation.navigate("Login")}>
+          Login
+        </AuthButton>
+        <Spacer size="large" />
+        <AuthButton onPress={() => navigation.navigate("Register")}>
+          Register
+        </AuthButton>
+      </AccountContainer>
     </AccountBackground>
   );
 };
