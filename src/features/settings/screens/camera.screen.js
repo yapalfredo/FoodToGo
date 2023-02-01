0e7473a25bbe7492790eb1bf37f2b771acf54efa
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-01-31 23:40:38 -0500

    implemented avatar image change from camera

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,21 +1,37 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useContext } from "react";
 import { Camera, CameraType } from "expo-camera";
 import styled from "styled-components/native";
 import { View } from "react-native";
+import { Button } from "react-native-paper";
 import { Text } from "../../../components/typography/text.component";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
   flex: 1;
 `;
 
 const CameraContainer = styled(View)`
+  width: 100%;
+  height: 100%;
   flex: 1;
-  justify-content: center;
 `;
 
-export const CameraScreen = () => {
+const CameraButton = styled(Button).attrs({
+  mode: "contained",
+  icon: "camera",
+})`
+  position: absolute;
+  top: 90%;
+  align-self: center;
+`;
+
+export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+  const { user } = useContext(AuthenticationContext);
 
   useEffect(() => {
     (async () => {
@@ -24,18 +40,30 @@ export const CameraScreen = () => {
     })();
   }, []);
 
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
+
   if (hasPermission === null) {
     return <View />;
   }
+
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>Requires Access To Camera</Text>;
   }
+
   return (
     <CameraContainer>
       <ProfileCamera
         ref={(camera) => (cameraRef.current = camera)}
         type={CameraType.front}
-      ></ProfileCamera>
+        ratio={"16:9"}
+      />
+      <CameraButton onPress={snap}>snap</CameraButton>
     </CameraContainer>
   );
 };
--- src/features/settings/screens/settings.screens.js
+++ src/features/settings/screens/settings.screens.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useCallback } from "react";
 import { List, Avatar } from "react-native-paper";
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components";
@@ -7,6 +7,10 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { useFocusEffect } from "@react-navigation/native";
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -18,11 +22,33 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getProfilePicture(user);
+    }, [user])
+  );
+
   return (
     <SafeArea>
       <AvatarContainer>
         <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+          {!photo && (
+            <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+          )}
+          {photo && (
+            <Avatar.Image
+              size={180}
+              backgroundColor="#2182BD"
+              source={{ uri: photo }}
+            />
+          )}
         </TouchableOpacity>
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
