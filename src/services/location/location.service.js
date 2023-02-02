ee4646cb3840adb7a4d3c26092736f962ae30807
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-02-01 22:42:36 -0500

    modified gitignore

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,6 +1,7 @@
 import camelize from "camelize";
 
 export const locationRequest = (searchTerm) => {
+  //had to use ngrok forwarding url to make this thing work in development mode
   return fetch(
     `http://b372-2603-7000-e101-96dd-7c6b-7ab0-7fc7-4332.ngrok.io/mealstogo-82704/us-central1/geocode?city=${searchTerm}`
   ).then((response) => response.json());
