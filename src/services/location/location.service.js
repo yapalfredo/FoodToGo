d1fc36e2ebc95fc48549c41f22cbf75d04a876b1
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-02-01 23:49:46 -0500

    transfered mocks to firebase server

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -3,12 +3,11 @@ import camelize from "camelize";
 export const locationRequest = (searchTerm) => {
   //had to use ngrok forwarding url to make this thing work in development mode
   return fetch(
-    `http://b372-2603-7000-e101-96dd-7c6b-7ab0-7fc7-4332.ngrok.io/mealstogo-82704/us-central1/geocode?city=${searchTerm}`
+    `http://d310-2603-7000-e101-96dd-7c6b-7ab0-7fc7-4332.ngrok.io/mealstogo-82704/us-central1/geocode?city=${searchTerm}`
   ).then((response) => response.json());
 };
 
 export const locationTransform = (result) => {
-  console.log(result);
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,23 +1,26 @@
-import { mockImages, mocks } from "./mock";
+import { mockImages } from "../../../functions/places/mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("Not Found");
-    }
-    resolve(mock);
-  });
+// export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+//   return new Promise((resolve, reject) => {
+//     const mock = mocks[location];
+//     if (!mock) {
+//       reject("Not Found");
+//     }
+//     resolve(mock);
+//   });
+// };
+
+export const restaurantsRequest = (location) => {
+  //had to use ngrok forwarding url to make this thing work in development mode
+  return fetch(
+    `http://d310-2603-7000-e101-96dd-7c6b-7ab0-7fc7-4332.ngrok.io/mealstogo-82704/us-central1/placesNearby?location=${location}`
+  ).then((response) => response.json());
 };
 
 //added 2 attributes at the end of the array
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResult = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map(() => {
-      return mockImages[Math.floor(Math.random() * (mockImages.length - 1))];
-    });
-
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
